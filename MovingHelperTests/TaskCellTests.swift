99c801c456040dd85a15514e770c3daacb62fb2a
surapong suwanlee <mr.surapong.s@gmail.com>
2015-09-15 16:48:13 +0700

    Update grabbing cell for test

--- MovingHelperTests/TaskCellTests.swift
+++ MovingHelperTests/TaskCellTests.swift
@@ -31,45 +31,45 @@ class TaskCellTests: XCTestCase {
         
         if let cell = testCell {
             //REST OF THE CODE BELOW SETTING UP THE CELL GOES HERE
+            //1
+            let expectation = expectationWithDescription("Task updated")
+            
+            //2
+            struct TestDelegate: TaskUpdatedDelegate {
+                let testExpectation: XCTestExpectation
+                let expectedDone: Bool
+                
+                init(updatedExpectation: XCTestExpectation,
+                    expectedDoneStateAfterToggle: Bool) {
+                        testExpectation = updatedExpectation
+                        expectedDone = expectedDoneStateAfterToggle
+                }
+                
+                func taskUpdated(task: Task) {
+                    XCTAssertEqual(expectedDone, task.done, "Task done state did not match expected!")
+                    testExpectation.fulfill()
+                }
+            }
+            
+            //3
+            let testTask = Task(aTitle: "TestTask", aDueDate: .OneMonthAfter)
+            XCTAssertFalse(testTask.done, "Newly created task is already done!")
+            cell.delegate = TestDelegate(updatedExpectation: expectation,
+                expectedDoneStateAfterToggle: true)
+            cell.configureForTask(testTask)
+            
+            //4
+            XCTAssertFalse(cell.checkbox.isChecked, "Checkbox checked for not-done task!")
+            
+            //5
+            cell.checkbox.sendActionsForControlEvents(.TouchUpInside)
+            
+            //6
+            XCTAssertTrue(cell.checkbox.isChecked, "Checkbox not checked after tap!")
+            waitForExpectationsWithTimeout(1, handler: nil)
+            
         } else {
             XCTFail("Test cell was nil!")
         }
-        
-        //1
-        let expectation = expectationWithDescription("Task updated")
-        
-        //2
-        struct TestDelegate: TaskUpdatedDelegate {
-            let testExpectation: XCTestExpectation
-            let expectedDone: Bool
-            
-            init(updatedExpectation: XCTestExpectation,
-                expectedDoneStateAfterToggle: Bool) {
-                    testExpectation = updatedExpectation
-                    expectedDone = expectedDoneStateAfterToggle
-            }
-            
-            func taskUpdated(task: Task) {
-                XCTAssertEqual(expectedDone, task.done, "Task done state did not match expected!")
-                testExpectation.fulfill()
-            }
-        }
-        
-        //3
-        let testTask = Task(aTitle: "TestTask", aDueDate: .OneMonthAfter)
-        XCTAssertFalse(testTask.done, "Newly created task is already done!")
-        cell.delegate = TestDelegate(updatedExpectation: expectation,
-            expectedDoneStateAfterToggle: true)
-        cell.configureForTask(testTask)
-        
-        //4
-        XCTAssertFalse(cell.checkbox.isChecked, "Checkbox checked for not-done task!")
-        
-        //5
-        cell.checkbox.sendActionsForControlEvents(.TouchUpInside)
-        
-        //6
-        XCTAssertTrue(cell.checkbox.isChecked, "Checkbox not checked after tap!")
-        waitForExpectationsWithTimeout(1, handler: nil)
     }
 }
